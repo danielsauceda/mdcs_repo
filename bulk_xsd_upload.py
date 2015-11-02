db61ad6acea80d231972f83de2b68932a0c0c7a5
danielsauceda <danielsauceda34@gmail.com>
2015-11-02 11:53:32 -0600

    modification

--- bulk_xsd_upload.py
+++ bulk_xsd_upload.py
@@ -21,28 +21,43 @@ def makeMyDirs(folder_path):
 
 
 
-def get_files(path,ext=('.xsd'):
+def get_files(path,ext=('.xsd')):
     """ gets all file files recursively that has a certain extenstion"""
     file_list=[]
     for root,dirs,files in os.walk(path):
         for f in files:
               if f.endswith(ext):
-              file_list.append(os.path.join(root,f))
+                  file_list.append(os.path.join(root,f))
     #returns list of all files with path
     return file_list
 
-def template_upload(filePath,urlIP='127.0.0.1:8000', mdcsUser='admin', mdcsPass='admin' ):
+def template_upload(filePath, mdcsUser='admin', mdcsPass='admin' ):
     """ uploads an xsd template """
-    mdcsURL='http://127.0.0.1:8000' # your MDCS IP address
     schema_data= "" #some string of loaded xsd
     #TODO: finish payload
     #TODO establish title as none type
     payload = {"content" : [ schema_data], 
                "filename" : [ '%s'], 
-               "hash"=["unkown"], 
-               "title":["%s"] } % 
+               "hash":["unknown"], 
+               "title":["%s"] } % ('s')
+
+
+def type_upload(filePath, mdcsUser='admin', mdcsPass='admin' ):
+    """ uploads an xsd template """
+    schema_data= "" #some string of loaded xsd
+    #TODO: finish payload
+    #TODO establish title as none type
+    payload = {"content" : [ schema_data], 
+               "filename" : [ '%s'], 
+               "hash":["unknown"], 
+               "title":["%s"] } % ('s')
+
 
 def main():
+    urlIP='127.0.0.1:8000'
+    
+
+
     list_xsd_filePaths=get_files(huddle_path)
     for file_path in list_xsd_filePaths:
         shutil.copy(file_path,xsd_path)
